45eeae26ada1a02bc3d9163bfdf41f6481cce661
Saeed1112 <nullee.sa@gmail.com>
2023-05-03 11:07:02 +0430

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+        fontFamily: {
+            'hanken-grotesk': ['Hanken Grotesk', 'sans-serif']
+        }
+    },
+    plugins: [],
+}
